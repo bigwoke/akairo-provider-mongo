bd93ece5a9eef7113cacf0110072c2c3d30cb285
bigwoke <djk152@gmail.com>
2020-03-26 04:52:50 -0400

    change approach to barebones functionality

--- index.js
+++ index.js
@@ -1,238 +1,94 @@
 const { Provider } = require('discord-akairo');
-const { Guild } = require('discord.js');
 
 /**
  * A provider for the discord-akairo framework using the `mongodb` node driver.
+ * @param {MongoClient} client - MongoDB Client from `mongodb`.
+ * @param {string} dbName - Name of database to use.
+ * @param {string} [collName='settings'] - Name of settings collection.
  * @extends {Provider}
  */
 class MongoDBProvider extends Provider {
-  /**
-   * @param {MongoClient} mongoClient - MongoDB Client from `mongodb`.
-   * @param {string} dbName - Name of database to use.
-   */
-  constructor (mongoClient, dbName) {
+  constructor (client, dbName, collName = 'settings') {
     super();
 
     /**
-     * MongoDB client holding connection to database.
-     * @type {MongoClient}
-     */
-    this.mongoClient = mongoClient;
-
-    /**
-     * MongoDB database to store/receive settings.
-     */
-    this.db = mongoClient.db(dbName);
-
-    /**
-     * Akairo client reference.
-     * @type {AkairoClient}
-     * @readonly
-     */
-    Object.defineProperty(this, 'akairoClient', { value: null, writable: true });
-
-    /**
-     * Client listeners mapped by event name.
-     * @type {Map<string, Object>}
+     * Reference to MongoDB settings collection.
      * @private
      */
-    this.listeners = new Map();
+    this.settings = client.db(dbName).collection(collName);
   }
 
   /**
-   * Initialize connection to database, build cache map, and attach listeners.
-   * @return {void}
+   * Builds data cache collection from database.
+   * @returns {Collection} - Settings cache.
    */
   async init () {
-    const collection = await this.db.collection('settings');
+    const data = await this.settings.find();
 
-    collection.find().forEach(doc => {
-      const guild = doc.guild === 0 ? 'global' : doc.guild;
-      this.items.set(guild, doc.settings);
-
-      if (guild === 'global' || this.akairoClient.guilds.has(doc.guild)) {
-        this.setupGuild(guild, doc.settings);
-      }
+    data.forEach(doc => {
+      this.items.set(doc._id, doc);
     });
 
-    this.listeners
-      // TODO: Create listeners for these
-      .set('commandPrefixChange', (guild, prefix) => this.set(guild, 'prefix', prefix))
-      .set('commandStatusChange', (guild, command, status) => {
-        this.set(guild, `cmd-${command.id}`, status);
-      })
-      .set('categoryStatusChange', (guild, category, status) => {
-        this.set(guild, `cat-${category.id}`, status);
-      })
-      .set('guildCreate', guild => {
-        const settings = this.items.get(guild.id);
-        if (!settings) return;
-        this.setupGuild(guild.id, settings);
-      })
-      // New command registered
-      .set('load', (command, isReload) => {
-        if (isReload) return;
-        for (const [guild, settings] of this.items) {
-          if (guild === 'global' || this.akairoClient.guilds.has(guild)) {
-            this.setupGuildCommand(guild, command, settings);
-          }
-        }
-      })
-      .set('categoryRegister', category => {
-        for (const [guild, settings] of this.items) {
-          if (guild === 'global' || this.akairoClient.guilds.has(guild)) {
-            this.setupGuildCategory(guild, category, settings);
-          }
-        }
-      });
-
-    for (const [event, listener] of this.listeners) this.akairoClient.on(event, listener);
+    return this.items;
   }
 
   /**
-   * Gets a value from a specified guild.
-   * @param {Guild} guild - Guild to get value for.
+   * Gets a value from a specified entry.
+   * @param {string} id - Entry to get value for.
    * @param {string} key - The key to get.
-   * @param {string} defaultValue - Default value if null or undefined.
-   * @return {Object | string} - Setting object or defaultValue string.
+   * @param {any} [defaultValue] - Default value if null or undefined.
+   * @returns {any}
    */
-  get (guild, key, defaultValue) {
-    const guildID = this.getGuildID(guild);
-    if (this.items.has(guildID)) {
-      const settings = this.items.get(guildID);
-      return typeof settings[key] === 'undefined' ? defaultValue : settings[key];
+  get (id, key, defaultValue) {
+    if (this.items.has(id)) {
+      const value = this.items.get(id)[key];
+      return value === null ? defaultValue : value;
     }
+
     return defaultValue;
   }
 
   /**
-   * Sets a value for a specified guild.
-   * @param {Guild} guild - Guild to set the value for.
+   * Sets a value for a specified entry.
+   * @param {string} id - Entry to set the value for.
    * @param {string} key - The key to set.
    * @param {string} value - The value to set.
-   * @return {string} - The value that was set.
+   * @returns {Promise}
    */
-  async set (guild, key, value) {
-    const guildID = this.getGuildID(guild);
-    const settings = this.items.get(guildID) || {};
-    settings[key] = value;
-    this.items.set(guildID, settings);
+  set (id, key, value) {
+    const data = this.items.get(id) || {};
+    data[key] = value;
+    this.items.set(id, data);
 
-    await this.updateGuild(guildID, settings);
-    return value;
+    return this.settings.updateOne(
+      { _id: id },
+      { $set: { _id: id, [key]: value } },
+      { upsert: true }
+    );
   }
 
   /**
-   * Deletes a value of a specified guild.
-   * @param {Guild} guild - The guild to delete the value of.
+   * Deletes a value from a specified entry.
+   * @param {string} id - The id of entry delete the value of.
    * @param {string} key - The key to delete.
-   * @return {string | boolean} - The value that was deleted.
+   * @returns {Promise}
    */
-  async delete (guild, key) {
-    const guildID = this.getGuildID(guild);
-    const settings = this.items.get(guildID) || {};
-    const value = settings[key];
-    delete settings[key];
+  delete (id, key) {
+    const data = this.items.get(id);
+    if (!data) throw new Error('No entry in items collection for given ID.');
+    delete data[key];
 
-    await this.updateGuild(guildID, settings);
-    return value;
+    return this.settings.updateOne({ _id: id }, { $unset: { [key]: '' } });
   }
 
   /**
-   * Clears a guild entry.
-   * @param {Guild} guild - Guild to clear.
-   * @return {void}
+   * Clears an entry.
+   * @param {string} id - Entry ID to clear.
+   * @returns {Promise}
    */
-  async clear (guild) {
-    this.items.delete(this.getGuildID(guild));
-    await this.updateGuild(this.getGuildID(guild), {});
-  }
-
-  /**
-   * Applies settings to Guild, Command, and Category instances.
-   * @param {Guild} guild - Guild to setup.
-   * @param {Object} settings - Settings to apply.
-   * @return {void}
-   */
-  setupGuild (guild, settings) {
-    if (typeof guild !== 'string') throw new TypeError('Guild must be an ID or "global"');
-    guild = this.akairoClient.guilds.get(guild) || null;
-
-    if (typeof settings.prefix !== 'undefined') {
-      // If guild exists, set guild prefix. If not, the prefix is global.
-      if (guild) guild._commandPrefix = settings.prefix;
-      else this.akairoClient._commandPrefix = settings.prefix;
-    }
-
-    for (const command of this.akairoClient.commandHandler.modules.values()) {
-      this.setupGuildCommand(guild, command, settings);
-    }
-    for (const category of this.akairoClient.commandHandler.categories.values()) {
-      this.setupGuildCategory(guild, category, settings);
-    }
-  }
-
-  /**
-   * Applies settings to Guild or Command instances.
-   * @param {Guild} guild - Guild to setup.
-   * @param {Command} command - Command to setup.
-   * @param {Object} settings - Settings to apply.
-   * @return {void}
-   */
-  setupGuildCommand (guild, command, settings) {
-    if (typeof settings[`cmd-${command.id}`] === 'undefined') return;
-    if (guild) {
-      if (!guild._commandsEnabled) guild._commandsEnabled = {};
-      guild._commandsEnabled[command.id] = settings[`cmd-${command.id}`];
-    } else {
-      if (!command._globalEnabled) command._globalEnabled = true;
-      command._globalEnabled = settings[`cmd-${command.id}`];
-    }
-  }
-
-  /**
-   * Applies settings to Guild or Category instances.
-   * @param {Guild} guild - Guild to setup.
-   * @param {Category} category - Category to setup.
-   * @param {Object} settings - Settings to apply.
-   * @return {void}
-   */
-  setupGuildCategory (guild, category, settings) {
-    if (typeof settings[`cat-${category.id}`] === 'undefined') return;
-    if (guild) {
-      if (!guild._categoriesEnabled) guild._categoriesEnabled = {};
-      guild._categoriesEnabled[category.id] = settings[`cat-${category.id}`];
-    } else {
-      if (!category._globalEnabled) category._globalEnabled = true;
-      category._globalEnabled = settings[`cat-${category.id}`];
-    }
-  }
-
-  /**
-   * Updates settings of a guild in the database.
-   * @param {Guild} guild - Guild to update settings for.
-   * @param {Object} settings - Settings to upsert.
-   * @return {Object} - Result set of setting update.
-   * @private
-   */
-  async updateGuild (guild, settings) {
-    guild = guild === 'global' ? 0 : guild;
-
-    const collection = await this.db.collection('settings');
-    return collection.updateOne({ guild }, { $set: { guild, settings } }, { upsert: true });
-  }
-
-  /**
-   * Returns the ID of a given guild.
-   * @param {Guild} guild - Guild to get ID of.
-   * @return {number | string} - Guild ID or 'global'.
-   * @static
-   */
-  static getGuildID (guild) {
-    if (guild instanceof Guild) return guild.id;
-    if (guild === 'global' || guild === null) return 'global';
-    if (typeof guild === 'string' && !isNaN(guild)) return guild;
-    throw new TypeError('Guild must be a Guild instance, guild ID, "global", or null.');
+  clear (id) {
+    this.items.delete(id);
+    return this.settings.deleteOne({ _id: id });
   }
 }
 
